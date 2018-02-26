cb7031bf07c919e16fe6e127012be6a947bfb9b7
Spinu Vlad <spinu.vlad@gmail.com>
2018-02-26 13:45:32 +0200

    fd

--- src/js/main.js
+++ src/js/main.js
@@ -9,34 +9,35 @@ var showEn = document.querySelectorAll('.show-en');
 displayNone();
 displayBlock(showRo);
 
-for (var i = 0; i < showRo.length; i++){
-	showRo[i].style.display = 'block';
+for (var i = 0; i < showRo.length; i++) {
+    showRo[i].style.display = 'block';
 }
 
-function showLangRo(){
-	displayNone();
-	displayBlock(showRo);
+function showLangRo() {
+    displayNone();
+    displayBlock(showRo);
 }
 
-function showLangRu(){
-	displayNone();
-	displayBlock(showRu);
-}
-function showLangEn(){
-	displayNone();
-	displayBlock(showEn);
+function showLangRu() {
+    displayNone();
+    displayBlock(showRu);
 }
 
-function displayNone(){
-	for (var i = 0; i < shows.length; i++){
-	shows[i].style.display = 'none';
-	}
+function showLangEn() {
+    displayNone();
+    displayBlock(showEn);
 }
 
-function displayBlock(varLang){
-	for (var i = 0; i < showRo.length; i++){
-	varLang[i].style.display = 'block';
-	}
+function displayNone() {
+    for (var i = 0; i < shows.length; i++) {
+        shows[i].style.display = 'none';
+    }
+}
+
+function displayBlock(varLang) {
+    for (var i = 0; i < showRo.length; i++) {
+        varLang[i].style.display = 'block';
+    }
 }
 
 langs[0].addEventListener('click', showLangRo);
